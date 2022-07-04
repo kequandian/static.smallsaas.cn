cb7b1e7d1dd88cbfc8aec7a5fcbbba31841fbda7
cmj <2118188796@qq.com>
2022-07-04 09:47:29 +0800

    修改打包文件名

--- buyinday/.umirc.js
+++ buyinday/.umirc.js
@@ -27,10 +27,10 @@ export default {
   ignoreMomentLocale: true, // 忽略 moment 的 locale 文件
 
   chainWebpack (memo, { env, webpack, createCSSRule }) {
-    memo.output.set('path', require('path').resolve(__dirname, 'dist/model-ui'))
+    memo.output.set('path', require('path').resolve(__dirname, 'dist/model-buyinday'))
     memo.output.set('filename', 'bundle.js')
   },
 
   // outputPath: '/dist/dev-logs',
-  publicPath: process.env.NODE_ENV === 'production' ? './model-ui/' : '/',  //设置 dist/index.html 访问 js和css路径
+  publicPath: process.env.NODE_ENV === 'production' ? './model-buyinday/' : '/',  //设置 dist/index.html 访问 js和css路径
 }
