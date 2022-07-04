3cdc69640205233dcf5b89854444a013739ee277
cmj <2118188796@qq.com>
2022-07-04 09:35:44 +0800

    修改打包路径

--- buyinday/gen.js
+++ buyinday/gen.js
@@ -3,5 +3,5 @@ var fs = require('fs-extra');
 
 var distPath = path.resolve(process.cwd(), './dist');
 
-fs.moveSync(`${distPath}/model-buyinday/index.html`, `${distPath}/model-buyinday.html`);
-// fs.moveSync(`${distPath}/static/x`, `${distPath}/x`);
+// fs.moveSync(`${distPath}/model-ui/index.html`, `${distPath}/model-ui.html`);
+fs.moveSync(`${distPath}/static/x`, `${distPath}/x`);
