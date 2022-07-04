716c25fb5b2ec82d5417b584934f3de180380792
cmj <2118188796@qq.com>
2022-07-04 09:39:36 +0800

    修改打包文件

--- buyinday/gen.js
+++ buyinday/gen.js
@@ -3,5 +3,5 @@ var fs = require('fs-extra');
 
 var distPath = path.resolve(process.cwd(), './dist');
 
-// fs.moveSync(`${distPath}/model-ui/index.html`, `${distPath}/model-ui.html`);
-fs.moveSync(`${distPath}/static/x`, `${distPath}/x`);
+fs.moveSync(`${distPath}/model-ui/index.html`, `${distPath}/model-ui.html`);
+// fs.moveSync(`${distPath}/static/x`, `${distPath}/x`);
